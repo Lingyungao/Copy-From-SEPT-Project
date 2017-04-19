6a7d4f04d9c35f839612471732fef1644b6434cd
rmit-s3427467-Donghai-Liu <s3427467@student.rmit.edu.au>
2017-04-19 22:08:28 +1000

    Onwer Menu Frame

---
+++ src/application/OwnerMenuController.java
@@ -0,0 +1,76 @@
+package application;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class OwnerMenuController{
+	
+	MenuMain a = new MenuMain();
+	
+    @FXML
+    private JFXButton oldBooking;
+
+    @FXML
+    private JFXButton editEmployee;
+
+    @FXML
+    private JFXButton ViewTimetable;
+
+    @FXML
+    private JFXButton newBooking;
+
+    @FXML
+    private JFXButton viewEmployee;
+
+    @FXML
+    private JFXButton editTimetable;
+
+    @FXML
+    private JFXButton viewBooking;
+
+    @FXML
+    private JFXButton addEmployee;
+
+    @FXML
+    void addEmployee(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void editEmployee(ActionEvent event) {
+
+    }
+
+    @FXML
+    void newBooking(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editTimetable(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ViewTimetable(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewEmployee(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewBooking(ActionEvent event) {
+
+    }
+
+    @FXML
+    void oldBooking(ActionEvent event) {
+
+    }
+
+}
