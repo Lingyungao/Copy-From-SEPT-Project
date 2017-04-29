666e934ec1b5749cc2756311beb06a96f0d88038
rmit-s3427467-Donghai-Liu <s3427467@student.rmit.edu.au>
2017-04-29 19:07:55 +1000

    bug fix

--- src/application/MenuMain.java
+++ src/application/MenuMain.java
@@ -168,7 +168,7 @@ public class MenuMain extends Application {
 	public void showLogin() throws IOException
 	{
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(MenuMain.class.getResource("/application/Sample.fxml"));
+		loader.setLocation(MenuMain.class.getResource("/application/Login.fxml"));
 		AnchorPane loginmenu = loader.load();
 		Scene scene = new Scene(loginmenu);
         scene.getStylesheets().add("/application/application.css");
