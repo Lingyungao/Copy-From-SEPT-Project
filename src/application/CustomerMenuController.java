ff9fd27d3308530e12b165f750cbb5c9b33e8be6
SpZanG <spookzang@gmail.com>
2017-04-29 14:39:49 +1000

    Merge all changes

--- src/application/CustomerMenuController.java
+++ src/application/CustomerMenuController.java
@@ -1,5 +1,50 @@
 package application;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
 public class CustomerMenuController {
+	
+MenuMain a = new MenuMain();
+	
+	@FXML
+	private JFXButton TimeView;
+	
+	@FXML
+	private JFXButton EditDetail;
+	
+	@FXML
+	private JFXButton BookingView;
+	
+	@FXML
+	 private void timeView(ActionEvent event) throws Exception {
+
+		a.showCusBookM();
+	}
+	
+	@FXML
+	 private void editDetail(ActionEvent event) throws Exception {
+
+		a.showCusDetailM();
+		
+	}
+	
+	@FXML
+	 private void BookingView(ActionEvent event) throws Exception {
+
+		a.viewCusBookM();
+		
+	}
+	
+	
 
 }
