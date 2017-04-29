fe996369c03bff69b6b5860ec1f3158eba58276a
Lingyungao <s3468120@student.rmit.edu.au>
2017-04-29 18:10:47 +1000

    Customer part update
    update to new ui and menu page

--- src/application/LoginSystem2.java
+++ src/application/LoginSystem2.java
@@ -8,8 +8,8 @@ public class LoginSystem2 {
 	private static String pass = null;
 	private static String user = null;	
 	private static int userId;
-	public static int returnId;
 	private static int permission;
+	public static int returnId;
 	private static Connection LoginConn = null;
 	private static ResultSet rs = null;
 	private static Statement st = null;
@@ -23,7 +23,9 @@ public class LoginSystem2 {
 			// Connection LoginConn = null;
 
 			LoginConn = connection.connectDB(); // connect to the SQL
+
 			st = LoginConn.createStatement(); // create statement of it
+
 			rs = st.executeQuery("SELECT * FROM USERS where USERNAME = \'" + inputLogUser + "\'");
 			// Query function 1(unsafe. easy to inject)
 			while (rs.next()) {
@@ -31,18 +33,23 @@ public class LoginSystem2 {
 				user = rs.getString("USERNAME");
 				userId = rs.getInt("USER_ID");
 				permission = rs.getInt("PERMISSION");
+
 			}
 			returnId = userId;
+
 			// get username and password
 
 			if (user.equals(inputLogUser) && pass.equals(inputLogPass)) {
 				// compare with database. have to same both.
 				if (permission == 1) {
 					System.out.println("Login Succesful");
+					MenuMain.userId = userId;
+					MenuMain.premission = permission;
 					return 2;
 				} else if (permission == 2) {
 					System.out.println("Login Succesful");
-					
+					MenuMain.userId = userId;
+					MenuMain.premission = permission;
 					return 3;
 				} else {	
 					System.out.println("Invalid Input!");
@@ -53,8 +60,7 @@ public class LoginSystem2 {
 				System.out.println("The account is not exist or you do not register an account");
 				return 1;
 			}
-			
+
 		
 	}
-	
 }
