068bf0ed8f91a2c68b5404073a1665f6d387309b
SpZanG <spookzang@gmail.com>
2017-04-30 17:42:41 +1000

    Add comments for Validation function

--- src/application/CusHandleException.java
+++ src/application/CusHandleException.java
@@ -1,11 +1,26 @@
 package application;
 
+
+/**
+ * This class is working on the customer input validation.
+ *
+ * @author Harold Zang
+ */
 public class CusHandleException {
+	// The regular for match user's input
 	private static String regWrong = "^[a-zA-Z0-9]+$";
+	// Check Number Only
 	private static String regNumOnly = "^[0-9]+$";
+	// Check character only
 	private static String regLetOnly = "^[a-zA-Z]+$";
+	// Check Email is valid or not
 	private static String regMail = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
 	
+	/**
+     * Check Username is valid or not
+     * if not match, show error message
+     * @param User input username.
+     */
 	public static boolean UserNameInputCheck(String userInput) throws Exception {
 		if (userInput.length() <= 3 || userInput.length() >= 17) {
 			System.out.println("Sorry, user name length cannot less than 3 or more than 17");
@@ -17,6 +32,11 @@ public class CusHandleException {
 		return true;
 	}
 
+	/**
+     * Check password is valid or not
+     * if not match, show error message
+     * @param User input password.
+     */
 	public static boolean PasswordInputCheck(String userInput) throws Exception {
 		if (userInput.length() <= 5) {
 			System.out.println("Sorry, password length is too short. The password cannot less than 5 characters.");
@@ -33,6 +53,11 @@ public class CusHandleException {
 
 	}
 
+	/**
+     * Check firstname is valid or not
+     * if not match, show error message
+     * @param User input firstname.
+     */
 	public static void UserFuncNameInputCheck1(String userInput) throws Exception {
 		if (userInput.length() <= 2 || userInput.length() >= 17) {
 			System.out.println("Sorry, first name length cannot less than 3 or more than 17");
@@ -44,6 +69,11 @@ public class CusHandleException {
 		}
 	}
 
+	/**
+     * Check phone number is valid or not
+     * if not match, show error message
+     * @param User input phone number.
+     */
 	public static boolean UserFuncPhoneInputCheck(String userInput) throws Exception {
 		if (userInput.length() <= 5 || userInput.length() >= 15) {
 			System.out.println("Sorry, Phone number length cannot less than 5 or more than 15");
@@ -55,6 +85,11 @@ public class CusHandleException {
 		return true;
 	}
 	
+	/**
+     * Check edit phone number is valid or not
+     * if not match, show error message
+     * @param User input phone number.
+     */
 	public static boolean EditFuncPhoneInputCheck(int userInput) throws Exception {
 		if (userInput <= 50000 || userInput >= 1500000000) {
 			System.out.println("Sorry, Phone number length cannot less than 5 or more than 15");
@@ -63,6 +98,11 @@ public class CusHandleException {
 		return true;
 	}
 	
+	/**
+     * Check address is valid or not
+     * if not match, show error message
+     * @param User input address.
+     */
 	public static boolean UserFuncAddressInputCheck(String userInput) throws Exception {
 		if (userInput.length() <= 15 || userInput.length() >= 150) {
 			System.out.println("Sorry, Address length cannot less than 15 or more than 150, and cannot only input number or characters");
@@ -74,6 +114,13 @@ public class CusHandleException {
 		return true;
 	}
 
+	/**
+     * Check employee is valid or not
+     * if not match, show error message
+     * @param employee first name.
+     * @param employee last name.
+     * @param employee phone number.
+     */
 	public static void UserFuncTestSQL(String empFirst, String empLast, String empPhone) throws Exception {
 		if (empFirst.equals("") && empLast.equals("") && empPhone.equals("")) {
 			throw new Exception("Employee's information is empty.");
@@ -104,6 +151,11 @@ public class CusHandleException {
 		}
 	}
 	
+	/**
+     * Check email address is valid or not
+     * if not match, show error message
+     * @param User input email.
+     */
 	public static boolean EmailInputCheck(String userInput) throws Exception {
 		if (userInput.matches(regMail) == false) {
 			System.out.println("Sorry, The email is not valid. Please input valid email. e.g: 123@gmail.com");
